dc4c55ec276db483c5dc167d78690b87b4696569
equintero75 <qesteban@uninorte.edu.co>
2025-01-28 00:53:19 -0500

    First Project

---
+++ backend/routes/product.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { deleteProduct, getProduct, createProduct, updateProduct } from "../controller/product.controller.js";
+
+const router = express.Router();
+
+router.get("/", getProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
